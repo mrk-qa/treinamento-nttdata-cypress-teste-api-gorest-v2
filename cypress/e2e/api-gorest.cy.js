f7162cf22472453d5c498bd695c7ba9abd05c9ff
Marco Antonio <marco.ant.seg@gmail.com>
2023-07-18 19:42:00 -0300

    add testes com custom commands e teste de contrato

--- cypress/e2e/api-gorest.cy.js
+++ cypress/e2e/api-gorest.cy.js
@@ -1,130 +1,100 @@
 describe('Testes API GOREST - Cenários Positivos', () => {
   it('[CT01] GET - Buscar todos os usuários', () => {
-    cy.api({
-      method: 'GET',
-      url: 'https://gorest.co.in/public/v2/users',
-      headers: {
-        Authorization: 'Bearer ' //token here
-      }
-    }).should((response) => {
+    cy.get_all_users()
+      .should((response) => {
       expect(response.status).to.eq(200)
       expect(response.body).not.empty
+      expect(response.body[0]).to.have.all.keys('id', 'name', 'email', 'gender', 'status')
     })
   })
 
   it('[CT02] GET - Busca de usuário com filtro de ID', () => {
-    cy.api({
-      method: 'GET',
-      url: 'https://gorest.co.in/public/v2/users/3614604',
-      headers: {
-        Authorization: 'Bearer ' //token here
-      }
-    }).should((response) => {
+    cy.get_user_filter_id()
+      .should((response) => {
       expect(response.status).to.eq(200)
       expect(response.body).not.empty
+      expect(response.body).to.have.all.keys('id', 'name', 'email', 'gender', 'status')
     })
   })
 
   it('[CT03] GET - Busca de usuário com filtro de name', () => {
-    cy.api({
-      method: 'GET',
-      url: 'https://gorest.co.in/public/v2/users',
-      headers: {
-        Authorization: 'Bearer ' //token here
-      },
-      qs: {
-        name: 'Jagmeet Varma'
-      }
-    }).should((response) => {
+    cy.get_user_filter_name()
+      .should((response) => {
       expect(response.status).to.eq(200)
       expect(response.body).not.empty
+      expect(response.body[0]).to.have.all.keys('id', 'name', 'email', 'gender', 'status')
+      expect(response.body[0].name).to.include('Nimit Mahajan')
     })
   })
 
   it('[CT04] GET - Busca de usuário com filtro de email', () => {
-    cy.api({
-      method: 'GET',
-      url: 'https://gorest.co.in/public/v2/users',
-      headers: {
-        Authorization: 'Bearer ' //token here
-      },
-      qs: {
-        email: 'aditya_mukhopadhyay@hauck.example'
-      }
-    }).should((response) => {
+    cy.get_user_filter_email()
+      .should((response) => {
       expect(response.status).to.eq(200)
       expect(response.body).not.empty
+      expect(response.body[0]).to.have.all.keys('id', 'name', 'email', 'gender', 'status')
     })
   })
 
   it('[CT05] GET - Busca de usuário com filtro de gênero', () => {
-    cy.api({
-      method: 'GET',
-      url: 'https://gorest.co.in/public/v2/users',
-      headers: {
-        Authorization: 'Bearer ' //token here
-      },
-      qs: {
-        gender: 'female'
-      }
-    }).should((response) => {
+    cy.get_user_filter_gender()
+      .should((response) => {
       expect(response.status).to.eq(200)
       expect(response.body).not.empty
+      expect(response.body[0]).to.have.all.keys('id', 'name', 'email', 'gender', 'status')
     })
   })
 
   it('[CT06] GET - Busca de usuário com filtro de status', () => {
-    cy.api({
-      method: 'GET',
-      url: 'https://gorest.co.in/public/v2/users',
-      headers: {
-        Authorization: 'Bearer ' //token here
-      },
-      qs: {
-        status: 'active'
-      }
-    }).should((response) => {
+    cy.get_user_filter_status()
+      .should((response) => {
       expect(response.status).to.eq(200)
       expect(response.body).not.empty
+      expect(response.body[0]).to.have.all.keys('id', 'name', 'email', 'gender', 'status')
     })
   })
 
   it('[CT07] GET - Buscar todos os comentários', () => {
-    cy.api({
-      method: 'GET',
-      url: 'https://gorest.co.in/public/v2/comments',
-      headers: {
-        Authorization: 'Bearer ' //token here
-      },
-    }).should((response) => {
+    cy.get_all_comments()
+      .should((response) => {
       expect(response.status).to.eq(200)
       expect(response.body).not.empty
+      expect(response.body[0]).to.have.all.keys('id', 'post_id', 'name', 'email', 'body')
     })
   })
 
   it('[CT08] GET - Buscar todos os posts', () => {
-    cy.api({
-      method: 'GET',
-      url: 'https://gorest.co.in/public/v2/posts',
-      headers: {
-        Authorization: 'Bearer ' //token here
-      },
-    }).should((response) => {
+    cy.get_all_posts()
+      .should((response) => {
       expect(response.status).to.eq(200)
       expect(response.body).not.empty
+      expect(response.body[0]).to.have.all.keys('id', 'user_id', 'title', 'body')
     })
   })
 
   it('[CT09] GET - Buscar todos os TO DOs', () => {
-    cy.api({
-      method: 'GET',
-      url: 'https://gorest.co.in/public/v2/todos',
-      headers: {
-        Authorization: 'Bearer ' //token here
-      },
-    }).should((response) => {
+    cy.get_all_todos()
+      .should((response) => {
       expect(response.status).to.eq(200)
       expect(response.body).not.empty
+      expect(response.body[0]).to.have.all.keys('id', 'user_id', 'title', 'due_on', 'status')
+    })
+  })
+
+  it('[CT10] POST - Criar usuário', () => {
+    cy.post_user()
+      .should((response) => {
+      expect(response.status).to.eq(201)
+      expect(response.body).not.empty
+      expect(response.body).to.have.all.keys('id', 'name', 'email', 'gender', 'status')
+    })
+  })
+
+  it('[CT11] DELETE - Excluir usuário', () => {
+    cy.del_user()
+    .should((response) => {
+      expect(response.status).to.eq(204)
+      expect(response.body).empty
     })
   })
 })
